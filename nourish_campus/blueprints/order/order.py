77a57ac5de3c5986b3595ccd81d571b7d6fee65f
Jerry Qian <36783706+JQ100@users.noreply.github.com>
2024-02-22 13:13:05 -0800

    create order page blueprint

---
+++ nourish_campus/blueprints/order/order.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request, redirect
+from ...models.models import MealOrder
+from ...extensions import db
+
+order_bp = Blueprint("order", __name__, template_folder="templates")
+
+
+@order_bp.route("/order", methods=['POST', 'GET'])
+def order():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_order = MealOrder(name=name)
+
+        try:
+            db.session.add(new_order)
+            db.session.commit()
+            return redirect('/order')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        orders = MealOrder.query.order_by(MealOrder.created_at).all()
+        return render_template('order_index.html', orders=orders)
