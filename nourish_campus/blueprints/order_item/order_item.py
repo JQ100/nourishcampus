1ccbd59371574b4676c1cdf56683471dac435861
Jerry Qian <jerrybbtt@gmail.com>
2024-04-13 16:39:48 -0700

    display order items for current user

--- nourish_campus/blueprints/order_item/order_item.py
+++ nourish_campus/blueprints/order_item/order_item.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect
-from ...models.models import OrderItem
+from ...models.models import OrderItem, MealOrder
 from ...extensions import db
 from flask_login import current_user
 
@@ -24,8 +24,19 @@ def order_item():
             return 'There was an issue adding your task'
 
     else:
-        order_items = OrderItem.query.order_by(OrderItem.created_at).all()
-        return render_template('order_item_index.html', order_items=order_items)
+        # gets all the orders from the current user
+        orders = MealOrder.query.filter_by(
+            customer_id=current_user.id).order_by(MealOrder.created_at).all()
+
+        # get all the ordered items from the order ids of the orders of the current user.
+        order_ids = [order.id for order in orders]
+        all_order_items_for_user = []
+        for order_id in order_ids:
+            order_items = OrderItem.query.filter_by(
+                order_id=order_id).order_by(OrderItem.created_at).all()
+            all_order_items_for_user.extend(order_items)
+    
+        return render_template('order_item_index.html', order_items=all_order_items_for_user)
 
 @order_item_bp.route("/order_item/<int:order_id>", methods=['GET'])
 def get_order_items(order_id):
