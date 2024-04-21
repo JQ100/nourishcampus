3d1ae8a5d6724267647848468cf6a71f8a680325
Jerry Qian <36783706+JQ100@users.noreply.github.com>
2024-04-21 12:37:31 -0700

    update feature for menu item

--- nourish_campus/blueprints/menu_item/menu_item.py
+++ nourish_campus/blueprints/menu_item/menu_item.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template, request, redirect
 from ...models.models import MenuItem, Restaurant
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 from ...extensions import db
 from flask_login import current_user
 
 menu_item_bp = Blueprint("menu_item", __name__, template_folder="templates")
+
+class MenuUpdateForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
+    calories = StringField('Calories', validators=[DataRequired()])
+    submit = SubmitField('Update Menu Item')
 
 
 @menu_item_bp.route("/menu_item", methods=['POST'])
@@ -37,6 +46,33 @@ def get_menu_item(restaurant_id):
     # first menu_index template is passed in, second is the var representing db query
     # menu_items contains all menu item queries, then used in template loop traversing the menu items
     return render_template('menu_index.html', menu_items=menu_items, restaurant=restaurant)
+
+@menu_item_bp.route("/menu_item/update/<int:restaurant_id>/<int:menu_item_id>", methods=["GET", "POST"])
+def update_menu_item(restaurant_id, menu_item_id):
+    if request.method == "POST":
+        # POST
+        updateForm = MenuUpdateForm()
+        if updateForm.validate():
+            menu_item = MenuItem.query.filter_by(id=menu_item_id).first()
+            menu_item.name = updateForm.name.data
+            menu_item.price = updateForm.price.data
+            menu_item.calories = updateForm.calories.data
+
+            db.session.commit()
+            return redirect('/menu_item/{restaurant_id}')
+        else:
+            # print the form errors for debugging
+            print(updateForm.errors)
+            return "There are errors in your update menu item form."
+    else:
+        # GET
+        menu_item = MenuItem.query.filter_by(id=menu_item_id).first()
+        updateForm = MenuUpdateForm()
+        # pre-fill the data with the menu item info
+        updateForm.name.data = menu_item.name
+        updateForm.price.data = menu_item.price
+        updateForm.calories.data = menu_item.calories
+        return render_template('menu_update.html', update_form=updateForm)
 
 
 @menu_item_bp.route("/menu_item/delete/<int:restaurant_id>/<int:menu_item_id>")
