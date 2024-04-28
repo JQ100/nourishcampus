4e7b21114c0529ee3623d52d4356578b2f00a834
Jerry Qian <36783706+JQ100@users.noreply.github.com>
2024-04-27 20:33:42 -0700

    remove help

--- nourish_campus/__init__.py
+++ nourish_campus/__init__.py
@@ -8,7 +8,6 @@ from .blueprints.menu_item.menu_item import menu_item_bp
 from .blueprints.restaurant.restaurant import restaurant_bp
 from .blueprints.order.order import order_bp
 from .blueprints.order_item.order_item import order_item_bp
-from .blueprints.help.help import help_bp
 
 # from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
@@ -33,7 +32,6 @@ def create_app():
     app.register_blueprint(restaurant_bp)
     app.register_blueprint(order_bp)
     app.register_blueprint(order_item_bp)
-    app.register_blueprint(help_bp)
 
     return app
 
